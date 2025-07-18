b152bfb00bd6f048752729801436eae5333d554f
IMRAN AHMAD <95746830+imranah10@users.noreply.github.com>
2025-07-18 21:13:25 +0530

    Update Home.jsx
    added Hero compo

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import '../../App.css'
 import { Services } from './Services'
 import { LuxuryExpe } from './LuxuryExpe'
-
+import { Hero } from './Hero'
 export const Home = () => {
   return (
     <>
+       <Hero/>
       <div className='main_parent'>
         <Services />
         <LuxuryExpe/>
